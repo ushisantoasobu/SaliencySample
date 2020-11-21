e11c48d3243edc86487210b5591c1362aa3558b7
ushisantoasobu <babblemann.shunsee@gmail.com>
2020-11-21 13:36:50 +0900

    画像を表示するところまで

--- SaliencySample/ViewController.swift
+++ SaliencySample/ViewController.swift
@@ -13,9 +13,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
 
-    
+        imageView.image = #imageLiteral(resourceName: "sample1")
+    }
 }
 
