12f89cc75bcf5dc4989b838c75c234cfaf5a6e83
ushisantoasobu <babblemann.shunsee@gmail.com>
2020-11-21 13:57:42 +0900

    sampleコードを写経

--- SaliencySample/ViewController.swift
+++ SaliencySample/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Vision
 
 class ViewController: UIViewController {
 
@@ -16,6 +17,31 @@ class ViewController: UIViewController {
         super.viewDidLoad()
 
         wholeImageView.image = #imageLiteral(resourceName: "sample1")
+
+        some()
     }
+
+    private func some() {
+        print("start")
+        let image = #imageLiteral(resourceName: "sample1")
+        let handler = VNImageRequestHandler(cgImage: image.cgImage!, options: [:])
+        let request: VNImageBasedRequest = VNGenerateAttentionBasedSaliencyImageRequest()
+        request.revision = VNGenerateAttentionBasedSaliencyImageRequestRevision1
+
+        try? handler.perform([request])
+        guard
+            let result = request.results?.first,
+            let observation = result as? VNSaliencyImageObservation
+        else {
+            fatalError("missing result")
+        }
+
+        guard let objects = observation.salientObjects else { return }
+        for object in objects {
+            print(object.boundingBox)
+        }
+    }
+
+    
 }
 
