f5bad6004b0cf0e245be740a20e187e41f4bab8b
ushisantoasobu <babblemann.shunsee@gmail.com>
2020-11-21 13:27:34 +0900

    imageViewの準備

--- SaliencySample/ViewController.swift
+++ SaliencySample/ViewController.swift
@@ -9,11 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
 }
 
